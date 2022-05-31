1fc6fe3388aef273d10201438da6900e29c968dd
onam <onamkrverma@gmail.com>
2022-05-31 14:45:28 +0530

    edit script

---
+++ script.js
@@ -0,0 +1,15 @@
+const on = () => {
+    document.querySelector('#myimage').src='img/bulb on.png';
+    document.querySelector('#btn-show').innerHTML = ' You just Trun on the blub 💡'
+    
+}
+
+const off = () => {
+    document.querySelector('#myimage').src='img/bulb off.png';
+    document.querySelector('#btn-show').innerHTML = ' You just Trun off the bulb 💡'
+}
+
+
+btn1.addEventListener('click', on );
+btn2.addEventListener('click', off );
+
